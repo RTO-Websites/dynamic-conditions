b10ae905e13285326b9969d57ad0cd2c332bed40
Sascha Hennemann <s.hennemann@rto.de>
2019-08-15 13:20:00 +0200

    Make months and days hardcoded

--- trunk/Lib/Date.php
+++ trunk/Lib/Date.php
@@ -99,12 +99,20 @@ class Date {
      * @return array
      */
     public static function getMonthsTranslated() {
-        $monthList = self::getMonths();
-
+        $monthList = [];
         // translate monthlist by wordpress-lang
-        foreach ( $monthList as &$month ) {
-            $month = __( $month );
-        }
+        $monthList[1] = __( 'January' );
+        $monthList[2] = __( 'February' );
+        $monthList[3] = __( 'March' );
+        $monthList[4] = __( 'April' );
+        $monthList[5] = __( 'May' );
+        $monthList[6] = __( 'June' );
+        $monthList[7] = __( 'July' );
+        $monthList[8] = __( 'August' );
+        $monthList[9] = __( 'September' );
+        $monthList[10] = __( 'October' );
+        $monthList[11] = __( 'November' );
+        $monthList[12] = __( 'December' );
 
         return $monthList;
     }
@@ -116,9 +124,18 @@ class Date {
      */
     private static function getMonths() {
         $monthList = [];
-        for ( $i = 1; $i <= 12; ++$i ) {
-            $monthList[$i] = strftime( '%B', mktime( 0, 0, 0, $i, 1 ) );
-        }
+        $monthList[1] = 'January';
+        $monthList[2] = 'February';
+        $monthList[3] = 'March';
+        $monthList[4] = 'April';
+        $monthList[5] = 'May';
+        $monthList[6] = 'June';
+        $monthList[7] = 'July';
+        $monthList[8] = 'August';
+        $monthList[9] = 'September';
+        $monthList[10] = 'October';
+        $monthList[11] = 'November';
+        $monthList[12] = 'December';
 
         return $monthList;
     }
@@ -129,12 +146,16 @@ class Date {
      * @return array
      */
     public static function getDaysTranslated() {
-        $dayList = self::getDays();
+        $dayList = [];
 
         // translate by wordpress-lang
-        foreach ( $dayList as &$day ) {
-            $day = __( $day );
-        }
+        $dayList[1] = __( 'Monday' );
+        $dayList[2] = __( 'Tuesday' );
+        $dayList[3] = __( 'Wednesday' );
+        $dayList[4] = __( 'Thursday' );
+        $dayList[5] = __( 'Friday' );
+        $dayList[6] = __( 'Saturday' );
+        $dayList[7] = __( 'Sunday' );
 
         return $dayList;
     }
@@ -146,12 +167,13 @@ class Date {
      */
     private static function getDays() {
         $dayList = [];
-        $year = date( 'o', time() );
-        $week = date( 'W', time() );
-        for ( $i = 1; $i <= 7; $i++ ) {
-            $time = strtotime( $year . 'W' . $week . $i );
-            $dayList[$i] = strftime( "%A", $time );
-        }
+        $dayList[1] = 'Monday';
+        $dayList[2] = 'Tuesday';
+        $dayList[3] = 'Wednesday';
+        $dayList[4] = 'Thursday';
+        $dayList[5] = 'Friday';
+        $dayList[6] = 'Saturday';
+        $dayList[7] = 'Sunday';
 
         return $dayList;
     }
