f30a8d5e5b4c3ae83245c8635437c5ee4469d334
Sascha Hennemann <s.hennemann@rto.de>
2021-04-02 12:01:39 +0200

    fix(plugin): Fix error if elementor is disabled

--- trunk/Lib/DynamicConditions.php
+++ trunk/Lib/DynamicConditions.php
@@ -208,7 +208,7 @@ class DynamicConditions {
      * @since 1.3.0
      */
     public function setFooterStyleForPreview() {
-        if ( !Plugin::$instance->preview->is_preview_mode() ) {
+        if ( !class_exists('Elementor\Plugin') || !Plugin::$instance->preview->is_preview_mode() ) {
             return;
         }
         ?>
